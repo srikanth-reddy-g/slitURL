98a88eddb47ff6ad312e325e9251287b11396f2a
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-10-04 23:56:55 -0400

    started building landing page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,5 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold ">
+        The only URL Shortener <br /> you&rsquo;ll ever need! 👇
+      </h2>
+      <form action="">
+        <Input />
+        <Button>Shorten!</Button>
+        {/* <img
+          src="/landing.jpeg"
+          alt="banner"
+          className="w-full my-11 md:px-11"
+        /> */}
+      </form>
+    </div>
+  );
 };
 
 export default LandingPage;

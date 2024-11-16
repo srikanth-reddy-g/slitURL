50c75912fef7415d163e172ad719e7a304c37572
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-11-16 04:02:58 -0500

    Add Fetch URL, Clicks, Delete, Create URL APIs, and Build Dashboard with Create URL Dialog

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -47,8 +47,10 @@ const Header = () => {
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
-                  <LinkIcon className="mr-2 h-4 w-4" />
-                  My Links
+                  <Link to="/dashboard" className="flex">
+                    <LinkIcon className="mr-2 h-4 w-4" />
+                    My Links
+                  </Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem className="text-red-400">
                   <LogOut className="mr-2 h-4 w-4" />
@@ -56,7 +58,7 @@ const Header = () => {
                     onClick={() => {
                       fnLogout().then(() => {
                         fetchUser();
-                        navigate("/auth");
+                        navigate("/");
                       });
                     }}
                   >

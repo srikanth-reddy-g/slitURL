d8f4474598ce73438d28944cd9037d414b76d858
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-11-18 00:25:35 -0500

    Deployment change

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: "0.0.0.0", // Bind to all network interfaces
+    port: parseInt(process.env.PORT) || 5173,
+  },
 });

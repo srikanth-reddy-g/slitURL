d652d59b1e887ad4b3b272924f43ea655d2b4168
Srikanth Reddy Gunukula <srikanthgunukula21@gmail.com>
2024-11-19 04:40:58 -0500

    Landing page UI Change

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -17,31 +17,32 @@ const LandingPage = () => {
     if (longUrl) navigate(`/auth?createNew=${longUrl}`);
   };
   return (
-    <div className="flex flex-col items-center">
-      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold ">
-        The only URL Shortener <br /> you&rsquo;ll ever need! 👇
-      </h2>
-      <form
-        onSubmit={handleShorten}
-        className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2"
+    <div className="flex flex-col items-center min-h-screen justify-between">
+      <div className="mt-36 mb-80">
+        <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold ">
+          The only URL Shortener <br /> you&rsquo;ll ever need! 👇
+        </h2>
+        <form
+          onSubmit={handleShorten}
+          className="sm:h-14 flex flex-col sm:flex-row w-full gap-2 px-4 md:px-0"
+        >
+          <Input
+            type="url"
+            value={longUrl}
+            placeholder="Enter your long URL"
+            onChange={(e) => setLongUrl(e.target.value)}
+            className="h-full flex-1 py-4 px-4"
+          />
+          <Button className="h-full" type="submit" variant="destructive">
+            Shorten!
+          </Button>
+        </form>
+      </div>
+      <Accordion
+        type="multiple"
+        collapsible="true"
+        className="w-full md:px-11 mb-10"
       >
-        <Input
-          type="url"
-          value={longUrl}
-          placeholder="Enter your long URL"
-          onChange={(e) => setLongUrl(e.target.value)}
-          className="h-full flex-1 py-4 px-4"
-        />
-        <Button className="h-full" type="submit" variant="destructive">
-          Shorten!
-        </Button>
-      </form>
-      <img
-        src="/image.png"
-        alt="banner"
-        className="w-2/5 h-80 my-11 md:px-11"
-      />
-      <Accordion type="multiple" collapsible className="w-full md:px-11">
         <AccordionItem value="item-1">
           <AccordionTrigger>
             How does this slitURL URL shortener works?
